9d05dca90bbc3aefa08073b9a687a02601312069
Jim Shaw <jimxshaw@gmail.com>
2015-11-16 16:05:11 -0500

    modified BaseballLeague creation SQL script

--- BaseballLeague/BaseballLeagueDatabaseWithTablesAndValues.sql
+++ BaseballLeague/BaseballLeagueDatabaseWithTablesAndValues.sql
@@ -14,6 +14,12 @@ create table Teams (
 )
 go
 
+create table Positions (
+	PositionID int identity(1,1) primary key,
+	PositionName nvarchar(50) not null
+)
+go
+
 create table Players (
 	PlayerID int identity(1,1) primary key,
 	FirstName nvarchar(50) not null,
@@ -22,44 +28,65 @@ create table Players (
 	YearsPlayed int not null,
 	BattingAvg decimal(3,3) null,
 	EarnedRunAvg decimal(5, 2) null,
-  TeamID int references Teams(TeamID) not null
+	TeamID int references Teams(TeamID) not null,
+	PositionID int references Positions(PositionID) not null
 )
 go
 
-create table Positions (
-	PositionID int identity(1,1) primary key,
-	PositionName nvarchar(50) not null,
-	PlayerID int references Players(PlayerID)
-)
-go
+
 
 insert into Teams (TeamName, ManagerName)
-	values ('Cleveland Indians', 'Tony La Russa'),
-		('Houston Astros', 'Manny Ramiriz'),
-		('New York Yankees', 'Joe Torre')
+	values ('Toronto Blue Jays', 'John Gibbons'),
+		('New York Yankees', 'Joe Girardi'),
+		('Baltimore Orioles', 'Buck Showalter'),
+		('Tampa Bay Rays', 'Santos Phelps'),
+		('Boston Red Sox', 'Oscar Webb'),
+
+		('Kansas City Royals', 'Caleb Bird'),
+		('Minnesota Twins', 'Patti Smith'),
+		('Cleveland Indians', 'Aaron Potter'),
+		('Chicago White Sox', 'Sherman Martin'),
+		('Detroit Tigers', 'Beth Wagner')
 go
 
-insert into Players(FirstName, LastName, JerseyNumber, YearsPlayed, BattingAvg, EarnedRunAvg, TeamID)
-	values ('Carlos', 'Gomez', 30, 8, .258, null, 1),
-		('Preston', 'Tucker', 20, 3, .203, null, 1),
-		('Scott', 'Benz', 7, 16, .172, 3.61, 1),
-		('Nate', 'Gurney', 6, 3, .344, null, 1),
-		('Jim', 'Shaw', 0, 9, .284, null, 1),
-		('Johnny', 'Baseball', 67, 3, .314, null, 1),
-		('Bob', 'Gibson', 42, 0, .301, null, 1),
-		('Abraham', 'Lincoln', 65, 17, .401, null, 1),
-		('Babe', 'Ruth', 3, 20, .346, 2.89, 1)
+insert into Positions (PositionName)
+	values ('SP'),
+		('RP'),
+		('CP'),
+		('C'),
+		('1B'),
+		('2B'),
+		('SS'),
+		('3B'),
+		('LF'),
+		('CF'),
+		('RF'),
+		('DH')
 go
 
-insert into Positions (PositionName, PlayerID)
-	values ('SP', 3),
-		('C', 1),
-		('1B', 2),
-		('2B', 4),
-		('SS', 5),
-		('3B', 6),
-		('LF', 7),
-		('CF', 8),
-		('RF', 9)
+insert into Players(FirstName, LastName, JerseyNumber, YearsPlayed, BattingAvg, EarnedRunAvg, TeamID, PositionID)
+	values ('Carlos', 'Gomez', 30, 8, .258, null, 1, 4),
+		('Preston', 'Tucker', 20, 3, .203, null, 2, 5),
+		('Scott', 'Benz', 7, 16, .172, 3.61, 3, 1),
+		('Nate', 'Gurney', 6, 3, .344, null, 4, 6),
+		('Jim', 'Shaw', 0, 9, .284, null, 5, 7),
+		('Johnny', 'Baseball', 67, 3, .314, null, 6, 8),
+		('Bob', 'Gibson', 42, 0, .301, null, 7, 9),
+		('Abraham', 'Lincoln', 65, 17, .401, null, 8, 10),
+		('Taylor', 'Moreno', 23, 2, .256, null, 9, 11),
+		('Cory', 'Johnson', 36, 6, .213, null, 1, 12),
+		('Darrin', 'Sunia', 31, 13, .137, null, 2, 4),
+		('Shelia', 'Simpson', 17, 8, .372, null, 3, 5),
+		('Joanna', 'Paul', 88, 5, .294, null, 4, 6),
+		('Claude', 'Jacob', 27, 3, .222, null, 5, 7),
+		('Jeff', 'Tebow', 55, 11, .118, null, 6, 8),
+		('Felix', 'Ryan', 11, 6, .294, 4.12, 7, 2),
+		('Kurt', 'Clapper', 7, 12, .302, 3.76, 8, 3),
+		('Rick', 'Wise', 36, 15, .312, null, 9, 5),
+		('Ben', 'King', 97, 5, .321, null, 1, 6),
+		('Luke', 'Green', 10, 0, .189, null, 2, 7),
+		('Spencer', 'Prince', 1, 18, .256, null, 3, 8)
 go
+
+
 
