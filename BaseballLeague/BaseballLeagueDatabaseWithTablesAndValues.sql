675300e93904dc35f2d987c126f68520159bb209
Patrick Beauchamp <patty.beauchamp@gmail.com>
2015-11-19 10:10:34 -0500

    restored deleted sprocs

--- BaseballLeague/BaseballLeagueDatabaseWithTablesAndValues.sql
+++ BaseballLeague/BaseballLeagueDatabaseWithTablesAndValues.sql
@@ -10,8 +10,16 @@ go
 use BaseballLeague
 go
 
+create table Leagues ( 
+ 	LeagueID int identity(1,1) primary key, 
+ 	LeagueName nvarchar(50) not null 
+) 
+go 
+
+
 create table Teams (
 	TeamID int identity(1,1) primary key,
+	LeagueID int foreign key references Leagues(LeagueID),
 	TeamName nvarchar(50) not null,
 	ManagerName nvarchar(100) not null,
 )
@@ -36,7 +44,9 @@ create table Players (
 )
 go
 
-
+insert into Leagues(LeagueName)
+	values ('MLB')
+go
 
 insert into Teams (TeamName, ManagerName)
 	values ('Toronto Blue Jays', 'John Gibbons'),
@@ -181,7 +191,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-create procedure [dbo].[NewJerseyNumber](
+
+CREATE procedure [dbo].[NewJerseyNumber](
 	@PlayerID int,
 	@TeamID int
 	)
@@ -203,16 +214,17 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-create procedure [dbo].[TradePlayer](
+
+CREATE procedure [dbo].[TradePlayer](
 	@PlayerID int,
-	@NewTeamID int,
-	@NewJerseyNumber int
+	@TeamID int,
+	@JerseyNumber int
 	)
 	as
 begin
 	update players
-	set TeamID = @NewTeamID, 
-		JerseyNumber = @NewJerseyNumber
+	set TeamID = @TeamID, 
+		JerseyNumber = @JerseyNumber
 	where PlayerID = @PlayerID
 end
 GO
@@ -272,5 +284,120 @@ BEGIN
 	select *
 	from Teams
 	where TeamID = @TeamID
+END
+GO
+
+--------------------------------------------
+
+USE [BaseballLeague]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[DeletePlayer](
+	@PlayerID int
+	)
+	as
+begin
+	delete from players
+	where PlayerID = @PlayerID
+end
+GO
+
+----------------------------------------------
+
+USE [BaseballLeague]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[GetTeamID](
+	@TeamName nvarchar(50)
+	)
+	as
+begin
+	select TeamID
+	from Teams
+	where TeamName = @TeamName
+end
+GO
+
+------------------------------------------------
+
+USE [BaseballLeague]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[GetPositionID](
+	@PositionName nvarchar(50)
+	)
+	as
+begin
+	select PositionID
+	from Positions
+	where PositionName = @PositionName
+end
+GO
+
+------------------------------------------------
+
+USE [BaseballLeague]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[AddNewPlayer](
+	@PositionID int,
+	@TeamID int,
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@JerseyNumber int,
+	@YearsPlayed int,
+	@BattingAverage decimal(3,3),
+	@EarnedRunAvg decimal(5,2),
+	@PlayerID int output
+	)
+	as
+begin
+	insert into Players (PositionID, TeamID, FirstName, LastName, JerseyNumber, YearsPlayed, BattingAvg, EarnedRunAvg)
+	values (@PositionID, @TeamID, @FirstName, @LastName, @JerseyNumber, @YearsPlayed, @BattingAverage, @EarnedRunAvg)
+
+	set @PlayerID = SCOPE_IDENTITY();
+end
+GO
+
+------------------------------------------
+
+USE [BaseballLeague]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[CreateTeam] 
+
+	@TeamName nvarchar(50), 
+	@ManagerName nvarchar(100),
+	@LeagueID int,
+	@TeamID int output 
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+	insert into Teams (LeagueID, TeamName, ManagerName)
+	values(@LeagueID, @TeamName, @ManagerName)
+	set @TeamID = Scope_Identity()
 END
 GO
