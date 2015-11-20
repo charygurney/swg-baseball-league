54b6d2152adf7ed9b789c98a28ca45b26ad20457
Chary Gurney <chary.gurney@gmail.com>
2015-11-20 15:37:57 -0500

    added jquery validation to add player

--- BaseballLeague/BaseballLeague.Models/Player.cs
+++ BaseballLeague/BaseballLeague.Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime;
 using System.Text;
@@ -10,13 +11,18 @@ namespace BaseballLeague.Models
     public class Player
     {
         public int PlayerID { get; set; }
+        [Required(ErrorMessage = "you must enter a first name.")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "you must enter a last name.")]
         public string LastName { get; set; }
         public int JerseyNumber { get; set; }
+        [Required(ErrorMessage = "you must enter years played.")]
         public int YearsPlayed { get; set; }
         public decimal? BattingAvg { get; set; }
         public decimal? EarnedRunAvg { get; set; }
+        [Required(ErrorMessage = "you must select a position.")]
         public string PositionName { get; set; }
+        [Required(ErrorMessage = "you must select a team.")]
         public string TeamName { get; set; }
 
         //public Player(int playerID, string firstName, string lastName, int jerseyNumber, int yearsPlayed, decimal battingAvg, decimal earnedRunAvg, string positionName, string teamName)
