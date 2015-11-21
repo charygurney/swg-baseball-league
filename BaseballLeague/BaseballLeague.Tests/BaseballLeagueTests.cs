283a3bae901376df01d4679fb3996a16a7cb66e1
Chary Gurney <chary.gurney@gmail.com>
2015-11-21 17:58:09 -0500

    added unit test for get all teams

--- BaseballLeague/BaseballLeague.Tests/BaseballLeagueTests.cs
+++ BaseballLeague/BaseballLeague.Tests/BaseballLeagueTests.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BaseballLeague.Data;
+using BaseballLeague.Data.Config;
+using NUnit.Framework;
 
 namespace BaseballLeague.Tests
 {
-    class BaseballLeagueTests
+    [TestFixture]
+   public  class BaseballLeagueTests
     {
+        [Test]
+        public void GetAllTeams_ShouldReturnCountOfTeams()
+        {
+            //Arrange
+            int expected = 0;
+            using (SqlConnection cn = new SqlConnection(Settings.ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "Select Count(*) From Teams";
+                cmd.Connection = cn;
+                //Act
+                cn.Open();
+                expected = int.Parse(cmd.ExecuteScalar().ToString());
+            }
+            //Assert
+            BaseballLeagueRepo bblRepo = new BaseballLeagueRepo();
+            Assert.AreEqual(expected, bblRepo.GetAllTeams().Count);
+        }
     }
 }
